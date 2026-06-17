cfdaf79395d7164fcbf0aeefdd2eecb83dd77306
Furina662 <gugushisanawa@gmail.com>
2026-06-17 12:04:32 +1200

    added standard code for calculated_list

--- calculate.py
+++ calculate.py
@@ -7,26 +7,31 @@ def calculate():
     }
     calculate_list = []
     for subject_name, subject_data in saved_grades_list.items():
-        for standard in subject_data.values():
-            if standard['grade'] in grade_points:
-                calculate_list.append({"subject": subject_name, 
-                                       "standard": standard})
+        for standard, standard_grade in subject_data.items():
+            if standard_grade['grade'] in grade_points:
+                calculate_list.append({
+                    "subject": subject_name, 
+                    "standard": standard,          
+                    "standard_grade": standard_grade 
+                })
 
     def get_point(item):
-        return grade_points.get(item['standard']['grade'], 0)
+        return grade_points.get(item['standard_grade']['grade'], 0)
 
     calculate_list.sort(key=get_point, reverse=True)
 
     total_credits = 0
     rank_score = 0
-    calculated_list = {}
-    for standard in calculate_list:
+    calculated_list = {} 
+    
+    for packet in calculate_list:
         if total_credits >= 80:
             break
 
-        grade = standard['standard']['grade']
-        credit = int(standard['standard']['credits'])
-        subject = standard['subject']
+        grade = packet['standard_grade']['grade']
+        credit = int(packet['standard_grade']['credits'])
+        subject = packet['subject']
+        standard = packet['standard']
         
         if grade in grade_points:
             if total_credits + credit > 80:
@@ -38,10 +43,12 @@ def calculate():
             rank_score += current_rank_score
 
             if subject not in calculated_list:
-                calculated_list[subject] = {'credits': 0, 'rank_score': 0}
+                calculated_list[subject] = {}
             
-            calculated_list[subject]['credits'] += credit
-            calculated_list[subject]['rank_score'] += current_rank_score
+            calculated_list[subject][standard] = {
+                'credits': credit,
+                'score': current_rank_score
+            }
 
     return (
         total_credits,
